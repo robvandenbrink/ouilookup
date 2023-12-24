94b3995657f878ab2a8172c1b84d7e5be090174f
Rob VandenBrink <rob@coherentsecurity.com>
2023-12-24 12:05:05 -0500

    Update oui-function.ps1

--- oui-function.ps1
+++ oui-function.ps1
@@ -1,5 +1,6 @@
 # documented at https://isc.sans.edu/forums/diary/Mining+Live+Networks+for+OUI+Data+Oddness/25404/
 # assumes that the oui.txt file exists, and that its in c:\utils - edit this to fit your implementation
+# run oui.cmd to collect the oui.txt file
 # declare this variable globally so that the OUILookup function doesn't have read the file each time it is called
 $global:ouilist = import-csv -Delimiter "`t" -path "c:\utils\oui.txt"
 
